99d0f409625e855be5f76306c75873f668b2eb2e
taman9333 <taman9333@gmail.com>
2021-03-06 21:26:33 +0200

    Make some changes in controller & grape entity to return same response after model changes

--- app/api/api/integrations.rb
+++ app/api/api/integrations.rb
@@ -11,7 +11,7 @@ module API
       end
       route_param :id do
         get do
-          integration = Integration.find(params[:id])
+          integration = Integration.eager_load(connections: :field_mappings).find(params[:id])
 
           present integration, with: API::Entities::Integration
         end
