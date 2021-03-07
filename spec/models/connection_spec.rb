725ce4d7cc1da0d4a098e2dcf5fe0381ae600172
GichohiKaruga <gichohi.karuga@outlook.com>
2021-03-07 13:14:36 +0300

    Updated connection spec

--- spec/models/connection_spec.rb
+++ spec/models/connection_spec.rb
@@ -1,29 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Connection, type: :model do
-  subject { Connection.new(integration_id: 1,auth_id: 1) }
+  subject { Connection.new(integration_id: 1, auth_id: 1) }
 
   it 'is not valid without an integration' do
     connection = Connection.new(integration_id: nil)
     expect(connection).to_not be_valid
   end
 
-  it 'is not valid without an external field' do
-    connection = Connection.new(integration_id: nil)
-    expect(connection).to_not be_valid
-  end
-
-  it 'is not valid without an integration' do
+  it 'is not valid without an auth' do
     connection = Connection.new(auth_id: nil)
     expect(connection).to_not be_valid
   end
 
-  it 'is not valid without an external field' do
-    connection = Connection.new(auth_id: nil)
-    expect(connection).to_not be_valid
-  end
-
-  it 'is valid with an integration' do
+  it 'is valid with all valid attributes' do
     expect(subject).to be_valid
   end
 
