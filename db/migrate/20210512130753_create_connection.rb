45896d187de5fd3c6e30e30a63d863911cd75fd6
Hossam Ahmed <hossam.elfar95@gmail.com>
2021-05-12 16:08:50 +0200

    🗃  adding connections and mapping fields migrations

---
+++ db/migrate/20210512130753_create_connection.rb
@@ -0,0 +1,11 @@
+class CreateConnection < ActiveRecord::Migration[5.2]
+  def change
+    create_table :connections do |t|
+      t.string :auth
+      t.string :path
+      t.references :integration
+
+      t.timestamps
+    end
+  end
+end
